1cef9171a35f866d306f1ff74b7faf1af9168567
EC2 Default User <ec2-user@ip-172-31-23-31.us-east-2.compute.internal>
2019-06-26 13:26:39 +0000

    finish13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
   def show
     @user=User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     #debugger
   end
   def new
@@ -52,13 +53,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+    
     
     # 正しいユーザーかどうか確認
     def correct_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -55,6 +56,9 @@ validates :password, presence: true, length: { minimum: 6 },allow_nil:true
   end
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+  def feed
+    Micropost.where("user_id = ?", id)
   end
    private
    # メールアドレスをすべて小文字にする
